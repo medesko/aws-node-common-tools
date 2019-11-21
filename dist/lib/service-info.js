3710a03017b81a1e89990113112e3cbc35d9429b
Mohamed Keita <mkeita@freelance.com>
2019-11-21 11:29:39 +0100

    rm dis module

--- dist/lib/service-info.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var _a;
-Object.defineProperty(exports, "__esModule", { value: true });
-const path = require("path");
-_a = require(path.join(process.cwd(), 'package.json')), exports.name = _a.name, exports.version = _a.version;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic2VydmljZS1pbmZvLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vLi4vbGliL3NlcnZpY2UtaW5mby50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7QUFBQSw2QkFBNkI7QUFDaEIsc0RBR1gsdURBQUMiLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgKiBhcyBwYXRoIGZyb20gJ3BhdGgnO1xuZXhwb3J0IGNvbnN0IHsgbmFtZSwgdmVyc2lvbiB9ID0gcmVxdWlyZShwYXRoLmpvaW4oXG5cdHByb2Nlc3MuY3dkKCksXG5cdCdwYWNrYWdlLmpzb24nXG4pKTtcbiJdfQ==
