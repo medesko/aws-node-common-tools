da8d53a143e8a5a359de89b2543fe4a065d20e9d
Mohamed Keita <mkeita@freelance.com>
2019-12-22 13:59:23 +0100

    refactoring render template

--- lib/nunjucks.render.ts
+++ lib/nunjucks.render.ts
@@ -1,18 +1,17 @@
+import * as path from 'path';
 import * as nunjucks from 'nunjucks';
 const dateFilter = require('nunjucks-date-filter');
-
+const views = path.join(__dirname + '/../views');
+console.log(views);
 const env = new nunjucks.Environment();
 env.addFilter('date', dateFilter);
-nunjucks.configure('views', { autoescape: true });
+nunjucks.configure(views, { autoescape: true });
 
-export const buildTemplate = (path: string, data: any = {}) => {
-	return new Promise<string>((resolve, reject) => {
+export const renderTemplate = (path: string, data: any = {}) => {
+	return new Promise<string | null>((resolve, reject) => {
 		return nunjucks.render(path, data, (error: any, template: string) => {
-			if (error !== null) {
-				return reject(error);
-			} else {
-				return resolve(template);
-			}
+			if (error !== null) return reject(error);
+			return resolve(template);
 		});
 	});
 };
