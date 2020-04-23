dd553ad2e10c321b856b717503da1cf5a294d6d7
Mohamed Keita <mkeita@freelance.com>
2020-04-24 00:22:07 +0200

    refactoring invoke

--- lib/invoke.lambda.ts
+++ lib/invoke.lambda.ts
@@ -1,38 +1,21 @@
 import * as AWS from 'aws-sdk';
-export const invoke = async (lambdaName, config) => {
+export const invoke = async ({ functionName, payload, config }) => {
 
-  const lambda: any = new AWS.Lambda({ region: config.region, apiVersion: '2015-03-31' });
+  config.apiVersion ? config.apiVersion : '2015-03-31';
 
-  const requestContext = config.clientId ? { authorizer: { claims: {client_id: config.clientId }}} : {}
-
-  const params: any =  {
-    FunctionName: `${lambdaName}`,
-    Payload: JSON.stringify({ body: config, requestContext}),
+  const params =  {
+    FunctionName: `${functionName}`,
+    Payload: JSON.stringify(payload),
     InvocationType: 'RequestResponse',
     LogType: 'Tail',
   }
 
-  return new Promise((resolve, reject) => {
-    lambda.invoke({
-      FunctionName: `${lambdaName}`,
-      Payload: JSON.stringify({
-        body: params,
-        requestContext,
-      }),
-      InvocationType: 'RequestResponse',
-      LogType: 'Tail',
-    }, (error, data) => {
-      if (error) {
-        return reject(error)
-      }
-      if (data.Payload) {
-        if (+JSON.parse(data.Payload).statusCode >= 400) {
-          return reject(JSON.parse(data.Payload))
-        }
-        const response = JSON.parse(data.Payload)
-
-        return resolve(response)
-      }
-    })
-  })
+  try {
+    const result: any = await new AWS.Lambda(config).invoke(params).promise();
+    const parsed: any = JSON.parse(result.Payload);
+    const body: any = JSON.parse(parsed.body);
+    return body;
+  } catch (err) {
+    return err;
+  }
 }
