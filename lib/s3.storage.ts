4fdad102a03f147b416dde191bf1ff8eb3258791
Mohamed Keita <mkeita@freelance.com>
2020-05-12 02:02:54 +0200

    added ssm config and s3 storage

---
+++ lib/s3.storage.ts
@@ -0,0 +1,23 @@
+import { S3, AWSError } from 'aws-sdk';
+import { PromiseResult } from 'aws-sdk/lib/request';
+import ReadableStream = NodeJS.ReadableStream;
+
+class S3Storage {
+
+  private s3: S3;
+  constructor(s3: S3) {
+      this.s3 = s3;
+  }
+
+  async put(object: ReadableStream, objectKey: string, bucket: string): Promise<PromiseResult<S3.PutObjectOutput, AWSError>> {
+      const putRequest = {
+        Body: object,
+        Bucket: bucket,
+        Key: objectKey
+      };
+
+      return await this.s3.putObject(putRequest).promise();
+  }
+}
+
+export { S3Storage };
