2cdb856088cc03c051f359f96b2ee2f057b86d7e
Mohamed KEITA <medesko@gmail.com>
2020-06-10 23:20:10 +0200

    added estlint

--- test/event.utilities.spec.ts
+++ test/event.utilities.spec.ts
@@ -1,40 +1,40 @@
 import { expect } from 'chai';
-const UUID = require('uuid/v5');
+import { v5 as UUID } from 'uuid';
 import { processEvent } from '../lib/event.utilities';
 
 describe('Service response test', async () => {
-	it('userId is extracted from the cognito lambda-proxy-mapped authorizer claims', async () => {
-		const testUserId = UUID;
-		const event = {
-			requestContext: {
-				authorizer: {
-					claims: {
-						'cognito:username': testUserId
-					}
-				}
-			}
-		};
+  it('userId is extracted from the cognito lambda-proxy-mapped authorizer claims', async () => {
+    const testUserId = UUID;
+    const event = {
+      requestContext: {
+        authorizer: {
+          claims: {
+            'cognito:username': testUserId,
+          },
+        },
+      },
+    };
 
-		const { userId } = processEvent(event);
-		expect(userId).to.equal(testUserId);
-	});
+    const { userId } = processEvent(event);
+    expect(userId).to.equal(testUserId);
+  });
 
-	it('body is parsed from the JSON body', async () => {
-		const testUserId = UUID;
-		const testBody = { a: 1, b: 2 };
-		const event = {
-			body: JSON.stringify(testBody),
-			requestContext: {
-				authorizer: {
-					claims: {
-						'cognito:username': testUserId
-					}
-				}
-			}
-		};
+  it('body is parsed from the JSON body', async () => {
+    const testUserId = UUID;
+    const testBody = { a: 1, b: 2 };
+    const event = {
+      body: JSON.stringify(testBody),
+      requestContext: {
+        authorizer: {
+          claims: {
+            'cognito:username': testUserId,
+          },
+        },
+      },
+    };
 
-		const { body, userId } = processEvent(event);
-		expect(userId).to.equal(testUserId);
-		expect(body).to.deep.equal(testBody);
-	});
+    const { body, userId } = processEvent(event);
+    expect(userId).to.equal(testUserId);
+    expect(body).to.deep.equal(testBody);
+  });
 });
--- test/sls.oauth.spec.ts
+++ test/sls.oauth.spec.ts
@@ -1,24 +1,22 @@
-import { expect } from 'chai';
-import { InitSlsOauth, SlsOauth  }  from '../lib/sls.oauth';
+// import { expect } from 'chai';
+// import { InitSlsOauth, SlsOauth } from '../lib/sls.oauth';
 
-const testSlsOauthConfig = {
-    "enabled": true,
-    "url": "https://develop-sls.auth.eu-central-1.amazoncognito.com/",
-    "accessToken": "",
-    "clientId": "sgni72n31b21952u9d8nt5mfq", // test app clientId
-    "clientSecret": "gc7qi7f549i1m7prtgjsj570ul1davdqnq8tkt69jahfj05nfbb",  // test app clientSecret
-    "code": "",
-    "redirectUri": "",
-    "verbose": false,
-    "debug": true
-
-	};
-
+// const testSlsOauthConfig = {
+//   enabled: true,
+//   url: 'https://develop-sls.auth.eu-central-1.amazoncognito.com/',
+//   accessToken: '',
+//   clientId: 'sgni72n31b21952u9d8nt5mfq', // test app clientId
+//   clientSecret: 'gc7qi7f549i1m7prtgjsj570ul1davdqnq8tkt69jahfj05nfbb', // test app clientSecret
+//   code: '',
+//   redirectUri: '',
+//   verbose: false,
+//   debug: true,
+// };
 
 describe('Service response test sls Oauth', async () => {
-	it('should authentificated client app to sls Oauth service', async () => {
+  it('should authentificated client app to sls Oauth service', async () => {
     // const slsOauth: SlsOauth = await InitSlsOauth(testSlsOauthConfig);
     // console.log(slsOauth)
-	// 	expect(slsOauth).haveOwnProperty('accessToken');
-	});
+    // 	expect(slsOauth).haveOwnProperty('accessToken');
+  });
 });
