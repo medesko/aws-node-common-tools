5fcaf721f2666e27ad8e0fef8af6cac3ebc3c7bd
Mohamed Keita <mkeita@freelance.com>
2020-12-09 10:03:15 +0100

    added joi validator

---
+++ lib/handler.wrapper.ts
@@ -0,0 +1,40 @@
+import * as Joi from 'joi';
+
+export const handlerWrapper = options => {
+  const { validate = {}, handler } = validateAndThrow(options, schema);
+
+  return async (e, ctx) => {
+    const eventWithDefaults = validateAndThrow(e, validate.e || {});
+    return handler(eventWithDefaults, ctx);
+  };
+};
+
+const validateAndThrow = (e, schema) => {
+  const { result, isValid, errors } = validateEvent(e, schema);
+  if (!isValid) {
+    throw new Error(errors.join(', '));
+  }
+
+  return result;
+};
+
+const validateEvent = (e, schema) => {
+  const result = schema.validate.validate(e, schema);
+  const isValid = result.error === null;
+  const errors = isValid ? null : result.error.details.map(detail => detail.message);
+
+  return { result: result.value, isValid, errors };
+};
+
+const compile = (v: any, options: any = { abortEarly: true }): Joi.ObjectSchema => {
+  return Joi.object(v).options(options);
+};
+
+const schema = {
+  validate: compile(
+    Joi.object({
+      e: Joi.object(),
+    }),
+  ),
+  handler: Joi.func().maxArity(2),
+};
