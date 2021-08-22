9766ccdd23f549457b65c8cf60c9a932c28bf193
Mohamed KEITA <medesko@gmail.com>
2021-08-23 00:46:30 +0200

    feat update

--- lib/log.utilities.ts
+++ lib/log.utilities.ts
@@ -1,10 +1,8 @@
 import * as path from 'path';
 import * as pino from 'pino';
 
-const { name } = require(path.join(process.cwd(), 'package.json'));
-
 export const log = pino({
-  name,
+  name: 'application',
   level:
     process.env.DEBUG || process.env.IS_OFFLINE || process.env.STAGE === 'develop'
       ? 'debug'
--- lib/nunjucks.render.ts
+++ lib/nunjucks.render.ts
@@ -1,13 +1,12 @@
 import * as path from 'path';
 import * as nunjucks from 'nunjucks';
-const views = path.join(__dirname + '/../../views');
+const views = path.join(__dirname + '/../../views/templates');
 nunjucks.configure(views, { autoescape: true });
 
-export const renderTemplate = (path: string, data: any = {}) => {
+export const renderTemplate = (templateName: string, mailData = {}) => {
   return new Promise<string | null>((resolve, reject) => {
-    return nunjucks.render(path, data, (error, template) => {
+    return nunjucks.render(templateName, mailData, (error, template) => {
       if (error !== null) return reject(error);
-      console.time();
       return resolve(template);
     });
   });
